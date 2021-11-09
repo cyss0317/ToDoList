fb7b45298abf666bead0e965ddf16cdd67db521c
Yun Sung Choi <cyss0317@gmail.com>
2021-11-08 22:31:37 -0600

    util functions

--- Frontend/src/util/todo_util.js
+++ Frontend/src/util/todo_util.js
@@ -1,1 +1,17 @@
 import axios from 'axios';
+
+export const createTodo = data => {
+    return axios.post('/api/todos/create', data)
+}
+
+export const updateTodo = todo => {
+    return axios.put(`/api/todos/${todo.id}`, todo)
+}
+
+export const deleteTodo = todoId => {
+    return axios.delete(`/api/todos/${todoId}`)
+}
+
+export const getTodo = todoId => {
+    return axios.get(`/api/todos/${todoId}`)
+}
