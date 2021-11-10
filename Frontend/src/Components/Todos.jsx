82b99faa29aa22b9c632a8b0260ad647f513cfb7
Yun Sung Choi <cyss0317@gmail.com>
2021-11-10 16:52:39 -0600

    CSS added

--- Frontend/src/Components/Todos.jsx
+++ Frontend/src/Components/Todos.jsx
@@ -18,27 +18,34 @@ const Todos = ({propTodos, title, status}) => {
     const [todo, setTodo] = useState({})
 
     useEffect( () => {
-        
-    },[todos])
+        setTodo({
+            description: newDescription,
+            done: "",
+            inProgress: "",
+            dueDate: "",
+            tag: []
+        })
+    },[newDescription])
 
 
-    console.log("status",status)
+    console.log("title",title)
 
     const statusTodo = status => {
-        if (status === "upcoming") {
-            //question,conditional redering not working
+        if (status === "Upcoming") {
+            //question, status conditional redering not working
             return (
                 <div >
                      Add Upcoming Todo
                 </div>
             )
-        } else if (status === "inProgress") {
+        } else if (status === "In Progress") {
+            console.log("gggggggg", status === "In Progress")
             return (
                 <div >
                     Add In Progress Todo
                 </div>
             )
-        } else if (status === "done") {
+        } else if (status === "Done") {
             return (
                 <div >
                     Add Done Todo
@@ -49,6 +56,7 @@ const Todos = ({propTodos, title, status}) => {
 
     const setDescriptionOnChange = (e) => {
         setNewDescription(e.target.value)
+        console.log(todo)
         // const newTodo = ({
         //     description: newDescription,
         //     dueDate: newDueDate,
@@ -87,18 +95,20 @@ const Todos = ({propTodos, title, status}) => {
     window.newDone = newDone
     const createSubmit = e => {
 
+        //question, it wouldn't rerender after creation
+
         // console.log("newDone from submit",newDone)
         // console.log("newProgress from submit",newProgress)
         // todoAPIUtil.createTodo(newTodo)
 
-        const newTodo = ({
-            description: newDescription,
-            dueDate: newDueDate,
-            done: status === "done" ? true : false,
-            inProgress: status === "inProgress" ? true : false,
-            tags: []
-        })
-        console.log("1231", newTodo)
+// const newTodo = ({
+//     description: newDescription,
+//     dueDate: newDueDate,
+//     done: status === "done" ? true : false,
+//     inProgress: status === "inProgress" ? true : false,
+//     tags: []
+// })
+        console.log("newTodo", todo)
         const modal = document.querySelector(".modal")
         modal.style.display = "none"
         setNewDone()
@@ -113,11 +123,12 @@ const Todos = ({propTodos, title, status}) => {
 
     // console.log("done",newDone)
     // console.log("prgress",newProgress)
-
+console.log("status", status)
 
     return (
         <div className="todos-container">
             <div className="title-addButton">
+{/* it's fine rendering title */}
                 <h1 className="title">{title}</h1>
                 <button onClick={e => openModal(e)} className="addTodo">+ Add new todo</button>
             </div>
@@ -129,8 +140,9 @@ const Todos = ({propTodos, title, status}) => {
             <div onClick={e => closeModal(e)} className="modal-background" style={{ display: "none" }}>
                 <div className="modal-child" onClick={e => e.stopPropagation()}>
                     <div className="status-x-button">
-                        {
-                            statusTodo(status)
+
+                        {   
+                            statusTodo(title)
                         }
                         <button onClick={e => closeModal(e)}id="modal-close-button" className="X-button">X</button>
                     </div>
