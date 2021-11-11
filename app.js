c53875463394ee5aaaaf6dbac4204fc5a82bc1e0
Yun Sung Choi <cyss0317@gmail.com>
2021-11-11 17:33:23 -0600

    heroku

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const db = require("./config/keys").mongoURI;
 const todos = require("./routes/api/todos");
 const path = require('path');
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'));
-//     app.get('/', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-// })
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+})}
 
 mongoose
     .connect(db, {useNewUrlParser: true})
